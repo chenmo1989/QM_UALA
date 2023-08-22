9efb16a9ff7530f458505dfb83de4218bac8b126
chenmo1989 <chenmo.sz@gmail.com>
2023-08-22 08:57:44 -0700

    UA Q3 single file update

--- update_machine.py
+++ update_machine.py
@@ -2,6 +2,8 @@ from quam import QuAM
 
 machine = QuAM('quam_state.json')
 
-machine.qubits[6].pi_length = 80
+machine.qubits[1].f_01 = 6214425658
+machine.qubits[1].lo = 6110E6
+#machine.qubits[1].pi_length = 80
 
 machine._save('quam_state.json', flat_data=False)
