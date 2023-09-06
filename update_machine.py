d225f6f83a47194554804498eee843a02e7b9d8c
chenmo1989 <chenmo.sz@gmail.com>
2023-09-06 13:44:53 -0700

    use pause for static bias

--- update_machine.py
+++ update_machine.py
@@ -3,8 +3,6 @@ from quam import QuAM
 machine = QuAM('quam_state.json')
 
 for i in range(7):
-    machine.qubits[i].x180_length = machine.qubits[i].pi_length
-    machine.qubits[i].x180_amp = machine.qubits[i].pi_amp
-    machine.flux_lines[i].Z_delay = 19.0
+    machine.resonators[i].readout_pulse_amp = 0.3
    
 machine._save('quam_state.json', flat_data=False)
