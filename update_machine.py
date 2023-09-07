5b1379745eef2e4a03390e98aea3cd1bacefd82a
chenmo1989 <chenmo.sz@gmail.com>
2023-09-06 18:50:17 -0700

    working on ESR with flux

--- update_machine.py
+++ update_machine.py
@@ -3,6 +3,6 @@ from quam import QuAM
 machine = QuAM('quam_state.json')
 
 for i in range(7):
-    machine.resonators[i].readout_pulse_amp = 0.3
+    machine.flux_lines[i].flux_pulse_amp = 0.25
    
 machine._save('quam_state.json', flat_data=False)
